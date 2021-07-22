fd93e0072f95d075b0f2be00053c2bf45d110d3b
Daxode <dax@daxode.dk>
2021-07-22 03:26:45 +0200

    Just some testing

---
+++ Editor/CampSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace DefaultNamespace {
+    #if GDC_EDITOR
+    [UnityEngine.CreateAssetMenu(fileName = "CampSettings", menuName = "MENUNAME")]
+    #endif
+    public class CampSettings : UnityEngine.ScriptableObject {
+        
+    }
+}
